8f83297422fbdf9a3ec9b0a7851e1dd501d5c008
Ifycode <marychord@gmail.com>
2021-10-16 08:14:00 +0100

    Events, refs and this binding

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,12 +2,29 @@ import React, { Fragment } from 'react';
 import { getFunName } from '../helpers'; //Already prepared helper/utility function
 
 class StorePicker extends React.Component {
+
+    myInput = React.createRef();
+
+    goToStore = event => {
+        //1. Prevent form from submitting (page reload)
+        event.preventDefault();
+        console.log('go to store...');
+        //2. Get text from input
+        console.log(this);
+        console.log(this.myInput);
+    }
     render() {
         return (
             <Fragment>
-                <form className="store-selector">
+                <form className="store-selector" onSubmit={this.goToStore}>
                     <h2>Please enter a store</h2>
-                    <input type="text" placeholder="Store Name" defaultValue={getFunName()} required/>
+                    <input
+                     type="text"
+                     ref={this.myInput}
+                     placeholder="Store Name" 
+                     defaultValue={getFunName()} 
+                     required
+                    />
                     <button type="submit">Visit Store ➡ </button>
                 </form>
             </Fragment>
