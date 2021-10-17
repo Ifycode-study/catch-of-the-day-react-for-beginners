ba59f9194d83c0ccfb2db501b2bc0a64aab12cc7
Ifycode <marychord@gmail.com>
2021-10-17 15:25:12 +0100

    Bi-directional data flow and live state editing

---
+++ src/components/EditFishForm.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class EditFishForm extends React.Component {
+    handleChange = event => {
+        //Update the fish
+        //1. Take a copy of fish
+        const updatedFish = {
+            ...this.props.fish,
+            [event.currentTarget.name]: event.currentTarget.value
+        } //correct this later: the price is back to being a string when input is edited - it should be a number
+
+        //updateFish prop and function from the app.js
+        this.props.updateFish(this.props.index, updatedFish);
+
+    };
+    render() {
+        return (
+            <div className="fish-edit">
+                <input type="text" name="name" onChange={this.handleChange} value={this.props.fish.name} />
+                <input type="text" name="price" onChange={this.handleChange} value={this.props.fish.price} />
+                <select name="status" onChange={this.handleChange} value={this.props.fish.status}>
+                    <option value="available">Fresh!</option>
+                    <option value="unavailable">Sold Out!</option>
+                </select>
+                <textarea name="desc" onChange={this.handleChange} value={this.props.fish.desc} placeholder="Desc"></textarea>
+                <input type="text" name="image" onChange={this.handleChange} value={this.props.fish.image} />
+                <button type="submit"> + Add Fish</button>
+            </div>
+        );
+    }
+}
+
+export default EditFishForm;
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import AddFishForm from './AddFishform';
+import EditFishForm from './EditFishForm';
 
 class Inventory extends React.Component {
     render() {
         return (
             <div className="inventory">
                 <h3>Inventory</h3>
+                {Object.keys(this.props.fishes).map(fishkey => (
+                    <EditFishForm key={fishkey} index={fishkey} fish={this.props.fishes[fishkey]} updateFish={this.props.updateFish}/>
+                ))}
                 <AddFishForm addFish={this.props.addFish}/>
                 <button onClick={this.props.loadSampleFishes}>Load Sample Fishes</button>
             </div>
